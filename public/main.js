0e0de05562eb9983f9a57cd70d4b6cac22291216
E'Gonzalez <ingenieria12@telemetrik.com.co>
2022-05-27 09:46:18 -0500

    mejoras en la imagen

--- public/main.js
+++ public/main.js
@@ -1,80 +1,95 @@
+//disable form submit
+$('form').submit(false);
 
-    //disable form submit
-    $('form').submit(false);
-
-    let device;
-    let port = navigator.serial;
-    let portOptions = {
+let device;
+let port = navigator.serial;
+let portOptions = {
     baudRate: 9600,
     dataBits: 8,
     parity: "none",
     stopBits: 1
-    }
+}
 
-    let baudRate = 9600;
+let baudRate = 9600;
 
-    port.onconnect = event => { console.log(event) };
-    port.ondisconnect = event => { console.log(event) };
+port.onconnect = event => {
+    console.log(event)
+};
+port.ondisconnect = event => {
+    console.log(event)
+};
 
-    async function deviceConnect(){
+async function deviceConnect() {
     await port.requestPort();
     await port.getPorts().then(devices => {
         console.log(devices);
-        
+
         device = devices[0];
 
         device.open(portOptions).then(result => {
-        console.log("Connected");
-        readPort();
-        $('#btn_connect').prop('disabled', true);
-        $('#masthead').addClass('d-none');
-        $('#card_control').removeClass('d-none');
+            console.log("Connected");
+            readPort();
+            $('#btn_connect').prop('disabled', true);
+            $('#masthead').addClass('d-none');
+            $('#card_control').removeClass('d-none');
         });
     })
-    }
+}
 
-    async function readPort(){
+async function readPort() {
     const textDecoder = new TextDecoderStream();
     const readableStreamClosed = device.readable.pipeTo(textDecoder.writable);
     const reader = textDecoder.readable.getReader();
 
     try {
         while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-            // |reader| has been canceled.
-            break;
-        }
-        // Do something with |value|...
-        console.log(value);
-        $('#txt_payload').val($('#txt_payload').val() + value);
-        $('#txt_payload').scrollTop($('#txt_payload')[0].scrollHeight);
+            const {
+                value,
+                done
+            } = await reader.read();
+            if (done) {
+                // |reader| has been canceled.
+                break;
+            }
+            // Do something with |value|...
+            console.log(value);
+            $('#txt_payload').val($('#txt_payload').val() + value);
+            $('#txt_payload').scrollTop($('#txt_payload')[0].scrollHeight);
         }
     } catch (error) {
         // Handle |error|...
     } finally {
         reader.releaseLock();
     }
-    }
+}
 
-    async function writePort(){
+async function writePort() {
     //let payload = "AT+SEND=2:000000000000000000000000000000000000000000000026";
-    let payload = $('#txt_sendPayload').val();
+    let payload = $('#txt_sendPayload').val().toUpperCase() ;
     payload += "\r\n";
     console.log(payload);
+
+    
+    $('#txt_payload').val($('#txt_payload').val() + ">> " + payload);
+    $('#txt_payload').scrollTop($('#txt_payload')[0].scrollHeight);
 
     const encoder = new TextEncoder();
     const writer = device.writable.getWriter();
     await writer.write(encoder.encode(payload));
     writer.releaseLock();
-    }
+}
 
-    async function closePort(){
+document.querySelector('#txt_sendPayload').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        writePort();
+    }
+});
+async function closePort() {
     await device.readable.releaseLock();
     await device.close();
-    }
+}
 
 
-    function getMsg(){
+function getMsg() {
     alert(document.getElementById('txt_payload').value)
-    }
+}
